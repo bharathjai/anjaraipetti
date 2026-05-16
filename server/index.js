2427622f5626897bd504b2d1299873ac1ff977e0
Bharath Jai <bharathjai2005@gmail.com>
2026-05-16 12:29:57 +0530

    fix: force IPv4 using nodemailer family option

--- server/index.js
+++ server/index.js
@@ -121,7 +121,11 @@ const razorpayClient =
     : null;
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  // Force IPv4 resolution
+  family: 4,
   auth: {
     user: process.env.GMAIL_USER || "",
     pass: process.env.GMAIL_PASS || ""
